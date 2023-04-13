8e7ecdbee64ae166728128294cd2330cb728eb0d
s1ismoil <130601812+s1ismoil@users.noreply.github.com>
2023-04-13 20:16:40 +0500

    Add files via upload

---
+++ my_first_param_function.py
@@ -0,0 +1,7 @@
+
+timer = 10
+def detonation_in(timer):
+    print(f"detonation in... {timer} secondes.")
+while (timer):
+  detonation_in(timer)
+  timer-=1
---
+++ my_first_return_function.py
@@ -0,0 +1,16 @@
+def my_get_seven():
+    return 7 
+
+print(my_get_seven())
+
+
+
+
+
+
+
+
+
+
+
+
